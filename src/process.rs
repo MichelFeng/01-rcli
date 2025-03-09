5724911df2752bf63a6d57700f33b49d355400fe
fengzhongshuang <fengzhongshuang@bytedance.com>
2025-03-09 20:29:24 +0800

    feature: support generic csv to json and yaml output
    Change-Id: I61254156b9aaf296dab69862eb0d37c5b6678c97

--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
-use csv::Reader;
+use csv::{Reader, StringRecord};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -15,21 +18,38 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
-    let mut rdr = Reader::from_path(input)?;
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
+    let mut reader: Reader<fs::File> = Reader::from_path(input)?;
     // let records = rdr
     //     .deserialize()
     //     .map(|record| record.unwrap())
     //     .collect::<Vec<Player>>();
 
-    let mut ret: Vec<Player> = Vec::with_capacity(128);
-    for result in rdr.deserialize() {
-        let record: Player = result?;
-        println!("{:?}", record);
-        ret.push(record);
+    let mut ret: Vec<Value> = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record: StringRecord = result?;
+        // headers.iter() -> ["name", "position", "DOB", "nationality", "Kit Number"]
+        // record.iter() -> ["Lionel Messi", "Forward", "24-06-1987", "Argentina", "10"]
+        // zip -> [("name", "Lionel Messi"), ("position", "Forward"), ...]
+        // collect::<Value>() -> {"name": "Lionel Messi", "position": "Forward", ...}
+        let value = headers.iter().zip(record.iter()).collect::<Value>();
+
+        // let iter = headers.iter().zip(record.iter());
+        // let value = match format {
+        //     OutputFormat::Json => iter.collect::<serde_json::Value>(),
+        //     OutputFormat::Yaml => iter.collect::<serde_yaml::Value>(),
+        //     OutputFormat::Toml => iter.collect::<toml::Value>(),
+        // };
+        ret.push(value);
     }
 
-    let json: String = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+        OutputFormat::Toml => toml::to_string(&ret)?,
+    };
+
+    fs::write(output, content)?;
     Ok(())
 }
